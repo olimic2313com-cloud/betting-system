155cfce071793cb4fef93bf68a4b204a9aa058ae
olimic2313com-cloud <olimic2313.com@gmail.com>
2026-05-24 12:41:12 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
+from backend.services.engine import run_engine
+
 app = FastAPI()
 
-@app.get("/bets")
-def bets():
-    return [
-        {
-            "player":"Bruno Fernandes",
-            "team":"Man United",
-            "opponent":"Chelsea",
-            "prob":0.63,
-            "odds":2.1,
-            "hit_rate":19,
-            "games":30,
-            "image":"https://cdn.sofifa.net/players/206/517/23_120.png"
+
+@app.get("/")
+def home():
+    return {
+        "status": "✅ Backend running",
+        "endpoints": [
+            "/run"
+        ]
+    }
+
+
+@app.get("/run")
+def run():
+    try:
+        bets = run_engine()
+        return bets
+
+    except Exception as e:
+        return {
+            "error": str(e)
         }
-    ]
