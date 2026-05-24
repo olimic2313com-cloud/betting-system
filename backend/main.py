7f6d678fc2e2e7f2e9e9955811a536b9baf0a050
olimic2313com-cloud <olimic2313.com@gmail.com>
2026-05-24 15:13:51 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,19 +3,10 @@ from backend.services.engine import run_engine
 
 app = FastAPI()
 
-
 @app.get("/")
 def home():
-    return {
-        "status": "✅ Backend running",
-        "endpoint": "/bets"
-    }
-
+    return {"status": "✅ Backend running", "endpoint": "/bets"}
 
 @app.get("/bets")
-def get_bets():
-    try:
-        return run_engine()
-    except Exception as e:
-        return {"error": str(e)}
-
+def bets():
+    return run_engine()
