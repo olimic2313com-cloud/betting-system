8dd822d13ef4b54edcb56fe07a3a703691fa07c6
olimic2313com-cloud <olimic2313.com@gmail.com>
2026-05-24 15:09:56 +0200

    Create player_stats.py

---
+++ backend/apis/player_stats.py
@@ -0,0 +1,30 @@
+import requests, os
+
+API_KEY = os.getenv("RAPIDAPI_KEY")
+
+def get_player_stats(player_id):
+
+    url = "https://sofasport.p.rapidapi.com/v1/players/statistics/result"
+
+    headers = {
+        "x-rapidapi-key": API_KEY,
+        "x-rapidapi-host": "sofasport.p.rapidapi.com"
+    }
+
+    params = {
+        "player_id": player_id,
+        "player_stat_type": "overall"
+    }
+
+    res = requests.get(url, headers=headers, params=params).json()
+
+    try:
+        stats = res["data"]
+
+        return {
+            "shots": stats.get("shotsTotal", 0),
+            "sot": stats.get("shotsOnTarget", 0)
+        }
+    except:
+        return {}
+``
