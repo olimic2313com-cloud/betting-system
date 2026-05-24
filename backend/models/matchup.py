6a0ef3bc84dc67b210df7c9205246f172a78cf7a
olimic2313com-cloud <olimic2313.com@gmail.com>
2026-05-24 15:22:13 +0200

    Delete backend directory

--- backend/models/matchup.py
+++
@@ -1,9 +0,0 @@
-def matchup_boost(att_pos, def_pos):
-
-    if att_pos in ["LW","RW"] and def_pos in ["RB","LB"]:
-        return 1.15
-
-    if att_pos == "ST" and def_pos == "CB":
-        return 1.1
-
-    return 1.0
--- backend/models/xg_model.py
+++
@@ -1,6 +0,0 @@
-def calc_xg(shots, sot):
-
-    if shots == 0:
-        return 0
-
-    return (shots * 0.05) + (sot * 0.25)
